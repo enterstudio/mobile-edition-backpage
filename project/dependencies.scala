b9eebaa15c50b5de5aa36a0acce61c081a69459f
Roberto Tyley <roberto.tyley@guardian.co.uk>
2013-10-24 15:46:14 +0100

    Update to latest version of content-api-client v2.7

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val clapper = "org.clapper" %% "grizzled-slf4j" % "1.0.1"
 
-  val contentApi = "com.gu.openplatform" %% "content-api-client" % "2.6"
+  val contentApi = "com.gu.openplatform" %% "content-api-client" % "2.7"
 
   // http://code.google.com/p/reflections/issues/detail?id=140
   val guardianManagementPlay = "com.gu" %% "management-play" % "5.26" exclude("javassist", "javassist")
