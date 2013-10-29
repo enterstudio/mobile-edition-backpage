8695ea9064dfc7739b26d3e22a3c2e9a9bb782ff
Roberto Tyley <roberto.tyley@guardian.co.uk>
2013-10-29 12:45:24 +0000

    Add cache-headers for 30 second cache

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -15,12 +15,19 @@ object Application extends Controller {
         technology <- LatestItemQuery.technology.response
         comment <- LatestItemQuery.commentIsFree.response
         mostViewed <- MostViewedQuery.all.response
-      } yield Ok(views.html.index(
+      } yield cacheHeaders(30, Ok(views.html.index(
         Story.singleStoryFrom(news),
         Story.singleStoryFrom(sport),
         Story.singleStoryFrom(technology),
         Story.singleStoryFrom(comment),
-        Story.mostViewedStoriesFrom(mostViewed)))
+        Story.mostViewedStoriesFrom(mostViewed))))
     }
+  }
+
+  private def cacheHeaders[A](seconds: Int, result: SimpleResult[A]) = {
+    // see http://www.ietf.org/rfc/rfc2616.txt & http://tools.ietf.org/html/rfc5861 for definitions of these headers
+    result.withHeaders(
+      "Cache-Control" -> s"max-age=$seconds, s-maxage=$seconds, stale-while-revalidate=$seconds, stale-if-error=345600"
+    )
   }
 }
